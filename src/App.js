2841504efa1c190213d9914896db5cfef8a5148a
Osama Abdul Latif <62595605+OsamaAbdellateef@users.noreply.github.com>
2022-08-24 02:08:34 +0200

    edit style extension

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+import "./App.scss";
 import Content from "./Content/Content";
 import Loader from "./Loader/Loader";
 import Navbar from "./Navbar/Navbar";
